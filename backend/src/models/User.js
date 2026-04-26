0a9ce64b34148c15dc6451214300ea71a18af8f3
ViniciusGCP94 <vinniegcp@gmail.com>
2026-04-25 22:08:55 -0300

    feat(models): implementa o model User.js com funções de busca e criação

---
+++ backend/src/models/User.js
@@ -0,0 +1,33 @@
+import pool from '../config/database.js';
+
+const criarUsuario = async (nome, email, senhaHash, role) => {
+    const res = await pool.query(`
+        INSERT INTO users (name, email, password, role)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, name, email, role`,
+        [nome, email, senhaHash, role]
+    );
+    return res.rows[0];
+}
+
+const buscarUsuarioPorEmail = async (email) => {
+    const res = await pool.query(`
+        SELECT * FROM users WHERE email = $1`, 
+        [email]
+    );
+    return res.rows[0];
+}
+
+const buscarUsuarioPorId = async (id) => {
+    const res = await pool.query(`
+        SELECT id, name, email, role FROM users WHERE id = $1`, 
+        [id]
+    );
+    return res.rows[0];
+}
+
+export {
+    criarUsuario,
+    buscarUsuarioPorEmail,
+    buscarUsuarioPorId
+};
