5752dd5bab59a05d3edafee5721295ea7ad4e764
ViniciusGCP94 <vinniegcp@gmail.com>
2026-04-26 00:54:20 -0300

    feat(auth): proteger rota de listagem de usuários com middlewares

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { criarUsuario, listarUsuarios } from '../controllers/userController.js'
+import { criarUsuario, listarUsuarios } from '../controllers/userController.js';
+import { autenticar, autorizar } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/registro', criarUsuario);
-router.get('/', listarUsuarios);
+
+router.get('/', autenticar, autorizar('defesa_civil'), listarUsuarios);
 
 export default router;
